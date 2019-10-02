2f7f84154450cb8a7daab94a2fc37caacf474fb8
Masaltzev <Masalcev_Andrey@Mail.ru>
2019-10-03 00:21:36 +0700

    gitignor

--- gulp/task/bs.js
+++ gulp/task/bs.js
@@ -1,7 +1,19 @@
 module.exports = function () {
     $.gulp.task('serve', function () {
         $.bs.init({
-            proxy: "build",
+            proxy: "build/",
+            notify:false
+            //port: 3000,
+            // startPath: "/php"
+            // server: {
+            //     baseDir: "./build"
+            // },
+            //startPath: "*.php"
+        });
+    });
+    $.gulp.task('serve', function () {
+        $.bs.init({
+            proxy: "build/",
             notify:false
             //port: 3000,
             // startPath: "/php"
