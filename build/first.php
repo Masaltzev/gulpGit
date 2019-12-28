ddb68f3276b2496908b0dcdf9f1e33d7da9d1e1a
Masaltzev <Masalcev_Andrey@Mail.ru>
2019-12-28 22:39:50 +0700

    card2

--- build/first.php
+++ build/first.php
@@ -15,6 +15,7 @@
           <ul class="navbar-nav">
             <li class="nav-item active"><a class="nav-link" href="first.php">Первая <span class="sr-only">current</span></a></li>
             <li class="nav-item"><a class="nav-link" href="type.php">Типы</a></li>
+            <li class="nav-item"><a class="nav-link" href=" cardCreate.php">Карточка</a></li>
             <li class="nav-item"><a class="nav-link" href="typeset.php">Верстаем</a></li>
             <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">php</a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"><a class="dropdown-item" href="db.php">Database</a><a class="dropdown-item" href="#">Another action</a><a class="dropdown-item" href="#">Something else here</a></div>
