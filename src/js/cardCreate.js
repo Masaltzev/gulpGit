daaf234051226393ed5e8de023759bcaeafea8e2
Masaltzev <Masalcev_Andrey@Mail.ru>
2020-01-10 22:09:31 +0700

    card64

--- src/js/cardCreate.js
+++ src/js/cardCreate.js
@@ -7,3 +7,13 @@ function showCounter () {
     this.style.display = 'none';
     counter.style.display='flex';
 }
+const counter = function () {
+    const btns = document.querySelectorAll('.counterBtn');
+    btns.forEach(btn=>{
+        btn.addEventListener('click', counterState())
+    });
+    function counterState() {
+
+    }
+};
+counter();
